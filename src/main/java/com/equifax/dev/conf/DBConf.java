f128a39532600d6c6ac1a13725c10944d0d6d47b
Henry Giovanni Martinez <henrygiovanni@gmail.com>
2020-10-14 14:18:15 -0600

    se modifico pom.xml y DBConfig para conexion de base de datos

--- src/main/java/com/equifax/dev/conf/DBConf.java
+++ src/main/java/com/equifax/dev/conf/DBConf.java
@@ -22,12 +22,20 @@ public class DBConf {
     private String hibernateDialect;
     @Value("${hibernate.show.sql}")
     private String hibernateShowSql;
+    @Value("${spring.datasource.username}")
+    private String usuario;
+    @Value("${spring.datasource.pasword}")
+    private String password;
+    @Value("${spring.datasource.port}")
+    private String port;
+    @Value("${spring.datasource.url}")
+    private String url;
 
     @Autowired
     @Bean(name = "sessionFactory")
     public SessionFactory sessionFactory(DataSource dataSource) throws Exception {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setDatabase(Database.ORACLE);
+        vendorAdapter.setDatabase(Database.POSTGRESQL);
         vendorAdapter.setGenerateDdl(false);
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setPackagesToScan("com.equifax.dev");
@@ -43,6 +51,10 @@ public class DBConf {
         properties.put("hibernate.dialect", hibernateDialect);
         properties.put("hibernate.show_sql", hibernateShowSql);
         properties.put("hibernate.enable_lazy_load_no_trans", "true");
+        properties.put("hibernate.user", usuario);
+        properties.put("hibernate.password", password);
+        properties.put("hibernate.port", port);
+        properties.put("hibernate.url", url);
         return properties;
     }
 
